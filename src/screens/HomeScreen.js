58b6887147c6a894736bc077499a39c8759f449a
treeoflife321 <scottpaurom@gmail.com>
2023-12-03 14:31:24 +0800

    first commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,78 @@
+import { View, Text, ScrollView, Image, TextInput } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import Categories from '../components/categories';
+import axios from 'axios';
+import Recipes from '../components/recipes';
+export default function HomeScreen() {
+
+  const [activeCategory, setActiveCategory] = useState('Beef');
+  const [categories, setCategories] = useState([]);
+  const [meals, setMeals] = useState([]);
+
+  useEffect(()=>{
+    getCategories();
+    getRecipes();
+  },[])
+
+  const handleChangeCategory = category=>{
+    getRecipes(category);
+    setActiveCategory(category);
+    setMeals([]);
+  }
+
+  const getCategories = async ()=>{
+    try{
+      const response = await axios.get('https://themealdb.com/api/json/v1/1/categories.php');
+      
+      if(response && response.data){
+        setCategories(response.data.categories);
+      }
+    }catch(err){
+      console.log('error: ',err.message);
+    }
+  }
+  const getRecipes = async (category="Beef")=>{
+    try{
+      const response = await axios.get(`https://themealdb.com/api/json/v1/1/filter.php?c=${category}`);
+      
+      if(response && response.data){
+        setMeals(response.data.meals);
+      }
+    }catch(err){
+      console.log('error: ',err.message);
+    }
+  }
+  return (
+    <View className="flex-1 bg-white">
+      <StatusBar style="dark" />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: 50}}
+        className="space-y-6 pt-14"
+      >
+
+        {/* greetings */}
+        <View className="mx-4 space-y-2 mb-2">
+          <View>
+            <Text style={{fontSize: hp(3.8)}} className="font-semibold text-neutral-600">Learn to cook food,</Text>
+          </View>
+          <Text style={{fontSize: hp(3.8)}} className="font-semibold text-neutral-600">
+            from <Text className="text-blue-400">home!</Text>
+          </Text>
+        </View>
+
+        {/* categories */}
+        <View>
+          { categories.length>0 && <Categories categories={categories} activeCategory={activeCategory} handleChangeCategory={handleChangeCategory} /> }
+        </View>
+
+        {/* recipes */}
+        <View>
+          <Recipes meals={meals} categories={categories} />
+        </View>
+      </ScrollView>
+    </View>
+  )
+}
